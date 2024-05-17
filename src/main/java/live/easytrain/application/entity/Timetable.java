a6c5b97fb57ca4398eaf10b28a2f735da6a71613
Calton Manhique <calton.manhique@dci-student.org>
2024-05-17 16:12:53 +0200

    API - binding data from api to entities classes in the ApiDataToEntities.java class

--- src/main/java/live/easytrain/application/entity/Timetable.java
+++ src/main/java/live/easytrain/application/entity/Timetable.java
@@ -16,9 +16,26 @@ public class Timetable {
     private String arrivalTime;
     private String departureTime;
 
+    /*
+    * Unique id provided by API to identify train scheduled time
+    * Is used to update the timetable requesting https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/rchg/{evaNo}
+    * */
+    private String scheduleId;
+
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "station_id")
     private Station station;
+
+    public Timetable(String startingPoint, String destination, String delay, String estimatedTripTime, String arrivalTime,
+                     String departureTime, String scheduleId) {
+        this.startingPoint = startingPoint;
+        this.destination = destination;
+        this.delay = delay;
+        this.estimatedTripTime = estimatedTripTime;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+        this.scheduleId = scheduleId;
+    }
 
     public Station getStation() {
         return station;
@@ -28,7 +45,6 @@ public class Timetable {
         this.station = station;
     }
 
-
     public Long getId() {
         return id;
     }
@@ -37,5 +53,59 @@ public class Timetable {
         this.id = id;
     }
 
+    public String getStartingPoint() {
+        return startingPoint;
+    }
 
+    public void setStartingPoint(String startingPoint) {
+        this.startingPoint = startingPoint;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDelay() {
+        return delay;
+    }
+
+    public void setDelay(String delay) {
+        this.delay = delay;
+    }
+
+    public String getEstimatedTripTime() {
+        return estimatedTripTime;
+    }
+
+    public void setEstimatedTripTime(String estimatedTripTime) {
+        this.estimatedTripTime = estimatedTripTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
 }
--- src/main/java/live/easytrain/application/external/binder/ApiDataToEntities.java
+++ src/main/java/live/easytrain/application/external/binder/ApiDataToEntities.java
@@ -2,8 +2,12 @@ package live.easytrain.application.external.binder;
 
 import live.easytrain.application.config.XmlResponse;
 import live.easytrain.application.entity.Timetable;
+import live.easytrain.application.external.entity.SType;
+import live.easytrain.application.external.entity.TimetableType;
+import live.easytrain.application.external.entity.TlType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +23,68 @@ public class ApiDataToEntities {
     }
 
     /*
-    * stationNumber = evaNo, so it is mandatory.
-    * */
-    public List<Timetable> apiDataToTimetable(Integer stationNumber, String date, String hour){
-        List<Timetable> timetables = new ArrayList<>();
+     * stationNumber = evaNo, so it is mandatory.
+     * recentChanges = true -> for Recent changes Timetables API requests and
+     * recentChanges = false -> for other requests
+     * */
+    public List<Timetable> apiDataToTimetable(Integer stationNumber, String date, String hour, boolean recentChanges) {
 
-        if (stationNumber == null){
+        List<Timetable> timetables = new ArrayList<>();
+        List<SType> sTypesICE = new ArrayList<>();
+        TimetableType timetableType = null;
+
+        if (stationNumber == null) {
             throw new IllegalArgumentException("stationNumber must not be null");
+        } else if (date.isEmpty() && hour.isEmpty() && !recentChanges) {
+            timetableType = xmlResponse.fetchXmlResponse("fchg" + "/" + stationNumber);
+        } else if (date.isEmpty() && hour.isEmpty()) {
+            timetableType = xmlResponse.fetchXmlResponse("rchg" + "/" + stationNumber);
+        } else {
+            timetableType = xmlResponse.fetchXmlResponse("plan" + "/" + stationNumber + "/" + date + "/" + hour);
+        }
+
+        for (SType sType : timetableType.getS()) {
+            TlType tlType = sType.getTl();
+            if (tlType != null && tlType.getC().equals("ICE")) {
+                sTypesICE.add(sType);
+            }
+        }
+
+        if (sTypesICE.isEmpty()) {
+            throw new RuntimeException("No ICE found on trajectory");
+        } else{
+            for(SType sTypeIce : sTypesICE){
+
+                String startingStation ="";
+                String endingStation = "";
+
+                // The train start stations are on <ar> tag and the destination on <dp> tag
+                // The stations are separated by | and regex doesn't like this special char
+                if(sTypeIce.getAr() == null) {
+                    startingStation = String.valueOf(stationNumber);
+                } else {
+                    String arStations = sTypeIce.getAr().getPpth().replace("|", "@");
+                    String[] arStartingStation = arStations.split("@");
+                    startingStation = arStartingStation[0];
+                }
+
+                if (sTypeIce.getDp() == null){
+                    endingStation = String.valueOf(stationNumber);
+                } else {
+                    String dpStations = sTypeIce.getDp().getPpth().replace("|", "@");
+                    String [] dpEndStation   = dpStations.split("@");
+                    endingStation = dpEndStation[dpEndStation.length-1];
+                }
+
+                String delay = "Störung"; // map the delay field
+                String estimatedTime = sTypeIce.getAr().getPt();
+                String arrivalTime = sTypeIce.getAr().getPt(); // To confirm we can request rchg/{evaNo}
+                String departureTime = sTypeIce.getDp().getPt();
+                String scheduledId = sTypeIce.getId();
+
+                timetables.add(new Timetable(startingStation, endingStation, delay, estimatedTime,arrivalTime,
+                        departureTime ,scheduledId));
+            }
         }
 
         return timetables;
