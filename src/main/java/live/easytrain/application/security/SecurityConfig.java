46cc57505e49a4c1f0b890b80ff93bb7442f79d6
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-05-20 17:46:36 +0200

    Add JDBC authentication

--- src/main/java/live/easytrain/application/security/SecurityConfig.java
+++ src/main/java/live/easytrain/application/security/SecurityConfig.java
@@ -1,4 +1,40 @@
 package live.easytrain.application.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class SecurityConfig {
+
+    private DataSource dataSource;
+
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT name, password, true FROM users WHERE name=?")
+                .authoritiesByUsernameQuery("SELECT name from roles WHERE name=?")
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                customizer -> customizer.requestMatchers("/").authenticated()
+        );
+
+        return httpSecurity.build();
+    }
 }
