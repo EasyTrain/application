0bf7c7cc9b966639621eb21b664a7b4050d029d8
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-05-23 12:16:50 +0200

    Add user registration logic, view

--- src/main/java/live/easytrain/application/controller/UserController.java
+++ src/main/java/live/easytrain/application/controller/UserController.java
@@ -1,13 +1,36 @@
 package live.easytrain.application.controller;
 
+import live.easytrain.application.entity.User;
+import live.easytrain.application.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@RestController
+@Controller
 public class UserController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration_form";
+    }
+
+    @PostMapping("/process_registration")
+    public String processRegistration(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        return "register_success";
     }
 }
