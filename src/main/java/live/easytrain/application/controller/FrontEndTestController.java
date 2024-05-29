60ac0c279ac4709bdd80571b3c759d22322c9c39
Kai Prager <kai.prager@kaiprager.eu>
2024-05-29 11:06:03 +0200

    Added Interfaces for StationRepo, TimetableRepo, BookingRepo, ConnectionRepo, TicketRepo

---
+++ src/main/java/live/easytrain/application/controller/FrontEndTestController.java
@@ -0,0 +1,14 @@
+package live.easytrain.application.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontEndTestController {
+
+@GetMapping("/frontend")
+public String showFrontEndTest() {
+    return "index";
+}
+
+}
