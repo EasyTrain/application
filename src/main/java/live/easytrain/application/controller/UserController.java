69b740ddf6bbd870ac00a6df25f61f10080a6193
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-10 15:34:33 +0200

    Add form validation to registration_form.html
    Mysterious exceptions were being thrown after form validation was added.
    
    The BindingResult parameter was moved to the second position in the
    method declaration in order to get rid of the exceptions these
    exceptions.

--- src/main/java/live/easytrain/application/controller/UserController.java
+++ src/main/java/live/easytrain/application/controller/UserController.java
@@ -2,6 +2,7 @@ package live.easytrain.application.controller;
 
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import live.easytrain.application.dto.UserRegistrationDto;
 import live.easytrain.application.entity.ChangePasswordRequest;
 import live.easytrain.application.entity.Email;
@@ -11,6 +12,7 @@ import live.easytrain.application.service.UserService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,10 +37,20 @@ public class UserController {
     }
 
     @PostMapping("/process_registration")
-    public String processRegistration(UserRegistrationDto userRegistrationDto,
-                                      HttpServletRequest request) throws UnsupportedEncodingException,
-            MessagingException {
-        userService.register(userRegistrationDto, getSiteURL(request));
+    public String processRegistration(@Valid @ModelAttribute("user") UserRegistrationDto userRegistrationDto, BindingResult bindingResult,
+                                      HttpServletRequest request
+            ) {
+
+        if (bindingResult.hasErrors()) {
+            return "registration_form";
+        }
+
+        try {
+            userService.register(userRegistrationDto, getSiteURL(request));
+
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            return "registration_form";
+        }
 
         return "register_success";
     }
--- src/main/java/live/easytrain/application/dto/UserRegistrationDto.java
+++ src/main/java/live/easytrain/application/dto/UserRegistrationDto.java
@@ -1,21 +1,31 @@
 package live.easytrain.application.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserRegistrationDto(
-        @NotEmpty(message = "Enter a first name")
+        @NotEmpty(message = "Enter first name")
+        @Size(min = 2, message = "First name must be at least 2 characters long")
         String firstName,
-        @NotEmpty(message = "Enter a last name")
+
+        @NotEmpty(message = "Enter last name")
+        @Size(min = 2, message = "Last name must be at least 2 characters long")
         String lastName,
-        @NotEmpty(message = "Enter an email address")
+
+        @NotEmpty(message = "Enter email address")
+        @Pattern(message = "Enter a valid email address",
+                regexp = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@"
+                        + "[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$")
         String email,
 
-        @NotEmpty(message = "Enter a phone number")
+        @NotEmpty(message = "Enter phone number")
         String phoneNumber,
-        @NotEmpty(message = "Enter a password")
+
+        @NotEmpty(message = "Enter password")
+        @Size(min = 8, message = "Password must be at least 8 characters long")
         String password,
-        @NotNull(message = "Passwords must match")
+//        @NotNull(message = "Passwords must match")
         String confirmPassword
 
 ) {
