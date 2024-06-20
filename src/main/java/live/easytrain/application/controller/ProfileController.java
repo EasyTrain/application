b6849d3f3ff2efbe87186083ccb46ce4d5add05f
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-20 12:03:22 +0200

    Add Edit profile view
    A user can edit their personal information. Age is not a required field
    but stree name, street number, postal code and city are. The validation
    on this form doesn't affect the user registration form.

--- src/main/java/live/easytrain/application/controller/ProfileController.java
+++ src/main/java/live/easytrain/application/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package live.easytrain.application.controller;
 
+import jakarta.validation.Valid;
 import live.easytrain.application.entity.User;
 import live.easytrain.application.repository.UserRepository;
 import live.easytrain.application.service.UserService;
@@ -7,7 +8,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProfileController {
@@ -22,14 +26,37 @@ public class ProfileController {
 
     @GetMapping("/profile")
     public String showProfile(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String principalName = authentication.getName();
-
-        User user = userService.getUserByEmail(principalName);
-        System.out.println(user);
-
-        model.addAttribute("user", user);
+        model.addAttribute("user", getUser());
 
         return "profile/profile";
     }
+
+    @GetMapping("/profile/edit")
+    public String showEditProfile(Model model) {
+        User user = getUser();
+
+        model.addAttribute("user", user);
+
+        return "profile/profile-edit";
+    }
+
+    @PostMapping("profile/edit")
+    public String editProfile(@Valid @ModelAttribute User user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println(bindingResult.getAllErrors());
+            return "profile/profile-edit";
+        }
+
+        userService.saveUser(user);
+        return "redirect:/profile";
+    }
+
+    private User getUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String principalName = authentication.getName();
+
+        return userService.getUserByEmail(principalName);
+    }
+
 }
